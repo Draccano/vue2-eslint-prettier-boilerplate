9e296198af77ee999c46a9235d46bb1e69d064d3
Adam Richter <draccanos@gmail.com>
2021-12-21 12:46:56 +0100

    git init

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,24 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/standard', '@vue/prettier'],
+  extends: [
+    'plugin:vue/essential',
+    'eslint:recommended',
+    '@vue/standard',
+    '@vue/prettier',
+  ],
   parserOptions: {
     parser: 'babel-eslint',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // 'import/extensions': ['warn', 'always', { vue: 'never', js: 'never' }],
+    'max-len': ['warn', { code: 120 }],
+    'func-names': ['warn', 'as-needed'],
+    'operator-linebreak': ['error', 'after'],
+    'linebreak-style': 0,
+    'comma-dangle': 'off',
+    'arrow-parens': ['warn', 'as-needed'],
   },
-}
+};
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Configuration of ESlint (7) + Standard.js + Prettier, asked on this SO question: https://stackoverflow.com/questions/68191278/vue-2-eslint-standard-prettier/68225547#68225547
+Configuration of ESlint (7) + Standard.js + Prettier
